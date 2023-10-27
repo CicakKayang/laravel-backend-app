94759d156bdd7d7cb13b7f886877f4f8a8d33c34
CicakKayang <kgaluhr@gmail.com>
2023-10-27 14:05:57 +0700

    meet 4

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -19,6 +19,18 @@ class UserSeeder extends Seeder
             'name' => 'Galuh Hasan',
             'email' => 'admin@gmail.com',
             'email_verified_at'  => now(),
+            'role' => 'admin',
+            'phone' => '6285226983111',
+            'bio' => 'flutter dev',
+            'password' => Hash::make('1234567890'),
+        ]);
+        User::create([
+            'name' => 'Super Admin',
+            'email' => 'superadmin@gmail.com',
+            'email_verified_at'  => now(),
+            'role' => 'superadmin',
+            'phone' => '6285666777888',
+            'bio' => 'laravel dev',
             'password' => Hash::make('1234567890'),
         ]);
     }
